cbb41e6cffde2b1fc0b8063f46822dc960c10788
WarrenMfg <warrenmfg@icloud.com>
2020-08-04 13:32:40 -0400

    update path to twitter image

--- src/components/DisplayQuotes.js
+++ src/components/DisplayQuotes.js
@@ -8,7 +8,7 @@ function DisplayQuotes({ globalStore: { state: { apiState: { quotes } } } }) {
         return (
           <div className='quote' key={i}>
             <a href={quote.href} target='_blank'>
-              <img src='twitter.png' width='180' height='180' />
+              <img src='../twitter.png' width='180' height='180' />
             </a>
             <p>{quote.quote}</p>
           </div>
