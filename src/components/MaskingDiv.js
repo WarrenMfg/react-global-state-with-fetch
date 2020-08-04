289a88f6f3ff2f8399118a42063f38ab1642c598
WarrenMfg <warrenmfg@icloud.com>
2020-08-04 13:21:59 -0400

    update twitter image location

--- src/components/MaskingDiv.js
+++ src/components/MaskingDiv.js
@@ -4,7 +4,7 @@ import withGlobalStore from './withGlobalStore';
 function MaskingDiv({ globalStore: { loading: { isLoading } } }) {
   return (
     <div className='masking-div' style={{ display: isLoading ? 'flex' : 'none' }}>
-      <img src='twitter.png' height='180' width='180' alt='Loading...' />
+      <img src='../src/twitter.png' height='180' width='180' alt='Loading...' />
     </div>
   );
 }
